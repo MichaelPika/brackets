46271ca7659aa6de8e33761ed8b53b706b8f6d08
MichaelPika <xauru7@gmail.com>
2022-02-03 00:33:22 +0200

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let count = 0;
+  let result;
+  for(i = 0; i < str.length; i++){
+    if(str[i] == "("){
+      count++;
+    } else if(str[i] == ")"){
+        if (count = 0){
+          result = false;
+        }else{
+          count--;
+        }
+    }
+  }
+  if(count == 0){
+    result = true;
+  }
+  return true; 
 }
